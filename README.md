2703dc72f52d4e019235977ea21e19b736fc2f25
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-11-18 19:34:14 +0100

    Bugfixes: bias towards maxnumber, wrong targetChange, improve run command

--- README.md
+++ README.md
@@ -11,6 +11,23 @@ A simulator for distributions following Benford's Law in Typescript
 
 ## Run
 
+You can generate a random number in a range with shuf, from coreutils library
+
 ```sh
-    ts-node src/index.ts --maxnumber 10000 --digitcount 2 --samplecount 500 --minnumber 1
+    ts-node src/index.ts --maxnumber $(shuf -i 10-100000 -n 1 | tee >(xargs echo "maxnumber=" 1>&2)) --digitcount 1 --samplecount 5000 --minnumber 1
+```
+
+The expected output would be like the following:
+
+```
+maxnumber= 10782
+1 | ==================== | 907
+2 | ===============      | 682
+3 | ==============       | 617
+4 | ============         | 558
+5 | ===========          | 512
+6 | ===========          | 478
+7 | ==========           | 453
+8 | =========            | 418
+9 | ========             | 375
 ```
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,15 @@ const debug:boolean=(argv.debug === "yes") ? true: false;
 
 const generateBenfordCompliantSample = function(maxNumber:number, sampleCount:number, minNumber:number=1) {
     let sample:Array<number>=[];
-    let targetChance:number=1/(maxNumber - minNumber);
-    for (let sampleSize:number=minNumber; sampleSize<=sampleCount; sampleSize++) {
-        for (let currentValue:number=1, maximumValue:number=maxNumber; currentValue <= maximumValue; currentValue++) {
-            // At the end of range, automatically push maximum number
-            if (currentValue === maximumValue) { sample.push(maximumValue); break; }
-            // Push number with targetChance probability
-            if (Math.random() < targetChance) { sample.push(currentValue); break; }
+    let targetChance:number=1/(maxNumber +1 - minNumber);
+    let currentLength:number
+    for (let sampleSize:number=1; sampleSize<=sampleCount; sampleSize++) {
+        currentLength=sample.length;
+        while (sample.length === currentLength) {
+            for (let currentValue:number=minNumber, maximumValue:number=maxNumber; currentValue <= maximumValue; currentValue++) {
+                // Push number with targetChance probability
+                if (Math.random() < targetChance) { sample.push(currentValue); break; }
+            }
         }
     }
     return sample
@@ -35,6 +37,7 @@ const getFirstNDigitsFromSample = function(sample:Array<number>, digitCount:numb
 const benfordLaw = function(maxNumber:number, sampleCount:number, digitCount:number=1, minNumber:number=0) {
     // console.log(`debug=${debug}, maxNumber=${maxNumber}, sampleCount=${sampleCount}, digitCount=${digitCount}, minNumber=${minNumber}`)
     let sample = generateBenfordCompliantSample(maxNumber=maxNumber, sampleCount=sampleCount, minNumber=minNumber);
+    if (debug) { process.stdout.write(String(sample) + '\n'); }
     let arrayOfFirstNDigits=getFirstNDigitsFromSample(sample=sample, digitCount=digitCount);
     return arrayOfFirstNDigits
 }
